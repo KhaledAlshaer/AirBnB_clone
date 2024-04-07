dc8559e686c05368d45fbb965111efb17c724d88
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-04-07 06:42:58 -0700

    4 fix

--- models/base_model.py
+++ models/base_model.py
@@ -17,7 +17,7 @@ class BaseModel:
                 if name == "__class__":
                     pass
                 elif name == "created_at" or name == "updated_at":
-                    value = datetime.datetime.strptime(value, tformat)
+                    value = datetime.datetime.strptime(value, self.tformat)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.today()
