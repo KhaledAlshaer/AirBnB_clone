2cfee8fb6cfe4c780e36598d0fe4e201984bab6d
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-04-07 06:38:08 -0700

    4

--- models/base_model.py
+++ models/base_model.py
@@ -18,10 +18,10 @@ class BaseModel:
                     pass
                 elif name == "created_at" or name == "updated_at":
                     value = datetime.datetime.strptime(value, tformat)
-                else:
-                    self.id = str(uuid.uuid4())
-                    self.created_at = datetime.datetime.today()
-                    self.updated_at = datetime.datetime.today()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.today()
+            self.updated_at = datetime.datetime.today()
 
     def __str__(self):
         """str method"""
